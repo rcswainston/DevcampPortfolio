24d1d90bcd982ef41ef4edc7bc526f0d40e6d555
Admin <administrator@Admins-MacBook-Pro.local>
2017-06-12 22:02:07 -0600

    Integrated new gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -55,3 +55,4 @@ gem 'devise', '~> 4.3'
 
 gem 'bootstrap', git: 'https://github.com/twbs/bootstrap-rubygem'
 gem 'jquery-rails'
+gem 'swainston_view_tool', git: 'https://github.com/rcswainston/swainston_view_tool'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,9 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    SwainstonViewTool::Renderer.copyright 'Cody Swainston', 'All rights reserved'
+  end
 end
+
